962635b8c54a388e66433e07d7fc2a21afef2ede
demstar16 <44187160+demstar16@users.noreply.github.com>
2023-08-24 16:54:02 +0800

    base django set up done
    complete the base (or front) 5 pages. None of the other pages work yet.

---
+++ cewa_site/cewa_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CewaAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cewa_app'
---
+++ cewa_site/cewa_app/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here.
+#from cewa_app.models import User
