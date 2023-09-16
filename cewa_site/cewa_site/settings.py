ca4187d623cd48b36355daabbd74668945b0c359
demstar16 <44187160+demstar16@users.noreply.github.com>
2023-09-16 09:45:00 +0800

    secured email key

--- cewa_site/cewa_site/settings.py
+++ cewa_site/cewa_site/settings.py
@@ -20,18 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-key = open("secret_key.txt", "r")
-SECRET_KEY = key.readlines()[0]
+api_key = open("api_key.txt", "r")
+SECRET_KEY = api_key.readlines()[0]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+email_key = open("email_key.txt", "r")
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'cleaningequipmentwa@gmail.com'
-EMAIL_HOST_PASSWORD = 'jzgf qecp qout szrz'
+EMAIL_HOST_PASSWORD = email_key
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
